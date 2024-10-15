e0bb857081403b19582864f1f8716f37ac474178
mrcurrlyhair <2124236@leedstrinity.ac.uk>
2024-10-15 14:19:40 +0100

    first

---
+++ Test1.py
@@ -0,0 +1,15 @@
+print('hello world')
+x = 2 
+y = 4
+print(x * y)
+
+
+
+num = 6
+if num > 0:
+    print(num, 'is a positive number')
+    print('the statement evealulates to True.')
+
+temperature = 30
+if temperature >25: 
+    print('its a hot day!')
