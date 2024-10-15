1a00960a7f1d47ca728df6a311e77517b7b8687d
mrcurrlyhair <2124236@leedstrinity.ac.uk>
2024-10-15 15:02:17 +0100

    test2

--- Test1.py
+++ Test1.py
@@ -26,3 +26,32 @@ count = 0
 while count < 5:  ## while loop stops counting after 5, stops at it would just continue on for infinity ##
     print(count)
     count += 1
+
+for i in range(10):
+    if i == 5 :
+        break #stops the loop when i = 5
+    print(i)
+
+for i in range (5):
+    if i ==2:
+        continue #skips 2
+    print(i)
+
+def opinion():
+    print('virtual machines are terrible')
+
+opinion() # output the definition 
+
+name = input('please enter your name.') # eneter name varaible 
+
+def greet(name):
+    print(f'hello,{name}!')    #definition for greeting 
+
+greet(name)  #print greet def and name 
+
+
+def greet2 (name2='Stranger'):
+    print(f'HELLO, {name2}!')
+
+greet2()
+
